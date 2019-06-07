04565ca235ee2812f3d03367080a23a7d6d3b455
claridiva2000 <claridiva2000@gmail.com>
2019-06-06 22:23:34 -0500

    done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const bcrypt = require('bcryptjs');
 
 const db = require('./database/dbConfig.js');
 const Users = require('./users/users-model.js');
@@ -17,7 +18,10 @@ server.get('/', (req, res) => {
 
 server.post('/api/register', (req, res) => {
   let user = req.body;
-
+  //generate a hash from  user's password
+  const hash = bcrypt.hashSync(user.password, 10); //2^n
+  //override user.password with hash
+  user.password = hash;
   Users.add(user)
     .then(saved => {
       res.status(201).json(saved);
@@ -33,7 +37,7 @@ server.post('/api/login', (req, res) => {
   Users.findBy({ username })
     .first()
     .then(user => {
-      if (user) {
+      if (user && bcrypt.compareSync(password, user.password)) {
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -44,12 +48,47 @@ server.post('/api/login', (req, res) => {
     });
 });
 
-server.get('/api/users', (req, res) => {
+function restricted(req, res, next) {
+  const { username, password } = req.headers;
+  // //make sure the entries arn't empty
+  //   if (username && password) {
+  //   } else {
+  //     res.status(400).json({ message: 'no credentials provided' });
+  //   }
+  if (username && password) {
+    Users.findBy({ username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          next();
+        } else {
+          res.status(401).json({ message: 'Invalid Credentials' });
+        }
+      })
+      .catch(error => {
+        res.status(500).json({ message: 'ran into a unexpected error' });
+      });
+  } else {
+    res.status(400).json({ message: 'no credentials provided' });
+  }
+}
+//protect this route, only authenticated users should see
+server.post('/api/users', restricted, (req, res) => {
   Users.find()
     .then(users => {
       res.json(users);
     })
     .catch(err => res.send(err));
+});
+
+//async version
+server.post('/users', restricted, async (req, res) => {
+  try {
+    const users = await Users.find();
+    res.json(users);
+  } catch (error) {
+    res.send(error);
+  }
 });
 
 const port = process.env.PORT || 5000;
